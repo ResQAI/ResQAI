6fbb99934c662f05fdf3ce06e9273905e1f3c6be
abhaydixit07 <abhaydixit227@gmail.com>
2024-12-09 16:16:33 +0530

    Make Profile more professional

--- src/app/profileTest/page.tsx
+++ src/app/profileTest/page.tsx
@@ -1,11 +1,13 @@
 "use client";
 
+import NationalHomeToggleArea from "@/components/National/NationalHomeToggle";
 import ProfileComponent from "@/components/Profile";
 
 export default function Profile() {
   return (
     <>
       <ProfileComponent />
+      <NationalHomeToggleArea />
       
     </>
   );
