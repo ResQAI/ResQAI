1e206da61404753c01a14c30772efd0f01808cb5
Shaurya Gupta <ShauryaGupta118@gmail.com>
2024-12-10 22:31:14 +0530

    removed dark

---
+++ src/app/ngo/Home/page.tsx
@@ -0,0 +1,23 @@
+// import NationalHomeToggleArea from "@/components/National/NationalHomeToggle";
+import NationalListSide from "@/components/Citizen/CitizenListSide";
+import ProfileComponent from "@/components/Profile"
+import React from "react";
+
+const page = () => {
+  return (
+    <div className="flex h-screen items-start justify-center">
+      {/* Left Half */}
+      <div className="filter-map-div h-full flex flex-col mt-12 w-[90%] bg-white">
+        <NationalListSide />
+        {/* <ProfileComponent /> */}
+      </div>
+      
+      {/* Right Half
+      <div className="toggle-area h-full flex flex-col w-[50%] bg-white">
+        <NationalHomeToggleArea />
+      </div> */}
+    </div>
+  );
+};
+
+export default page;
